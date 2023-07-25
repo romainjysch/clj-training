e0eb8df735e5311fcff25cf57e58f599b9cc0b63
Romain Jysch <rjh-network@pm.me>
2023-07-25 09:11:52 +0200

    refactor(clj-training): Change config function with the ->>

--- src/librairies/next_jdbc.clj
+++ src/librairies/next_jdbc.clj
@@ -1,14 +1,21 @@
 (ns librairies.next_jdbc
   (:require [next.jdbc :as jdbc]
             [next.jdbc.result-set :as rs]
-            [clojure.edn :as edn]
             [clojure.java.io :as io]))
 
-(def config (edn/read-string (slurp (io/resource "config.edn"))))
+(def config
+  (->> (io/resource "config.edn")
+       slurp
+       read-string))
+
 (def db-spec (:db-spec config))
+
+(println db-spec)
 
 (def all-restaurants (jdbc/execute! db-spec ["SELECT * FROM RESTAURANTS"]
                                     {:builder-fn rs/as-lower-maps}))
+
+(println all-restaurants)
 
 (defn vectorize-name-and-website [restaurants]
   (vec (map (fn [x] (select-keys x [:nom :site_web])) restaurants)))
