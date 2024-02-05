3ebc6bba7af34711a8c743c00cef7baa17951908
Romain Jysch <rjh-network@pm.me>
2024-02-05 17:59:05 +0100

    feat(clj-training): First experiment combining cheshire and aleph

--- src/librairies/http_server.clj
+++ src/librairies/http_server.clj
@@ -1,8 +1,15 @@
 (ns librairies.http_server
   (:require [ring.adapter.jetty :as jetty]
             [clj-http.client :as client]
-            [aleph.http :as http]))
+            [aleph.http :as http]
+            [cheshire.core :as json]))
 
+(def people
+  [{:756 {:firstname "Romain" :age 24 :city "Lausanne"}}
+   {:757 {:firstname "Victor" :age 26 :city "Lausanne"}}])
+(println people)
+
+;; text with ring :
 (defn handler [request]
   {:body "Hello Clojure!"
    :status 200
@@ -10,6 +17,13 @@
 
 (jetty/run-jetty handler {:port 12000 :join? false})
 
+;; json with cheshire :
+(defn handler [request]
+  {:body (json/generate-string people {:pretty true})
+   :status 200
+   :headers {"Content-Type" "application/json"}})
+
 (http/start-server handler {:port 12000 :join? false})
 
+;; client with clj-http :
 (client/get "http://localhost:12000")
