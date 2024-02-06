61e24abfa85c4d34bf27c4d72b1a4a6661e2f941
Romain Jysch <rjh-network@pm.me>
2024-02-06 09:19:11 +0100

    feat(clj-training): Change people for a map data structure

--- src/librairies/http_server.clj
+++ src/librairies/http_server.clj
@@ -2,12 +2,23 @@
   (:require [ring.adapter.jetty :as jetty]
             [clj-http.client :as client]
             [aleph.http :as http]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [librairies.jdbc :as da]))
 
-(def people
+(def people-vector
   [{:756 {:firstname "Romain" :age 24 :city "Lausanne"}}
    {:757 {:firstname "Victor" :age 26 :city "Lausanne"}}])
 (println people)
+
+(def people-map {:1 {:firstname "Romain"
+                     :age 24
+                     :city "Lausanne"}
+                 :2 {:firstname "Robin"
+                     :age 24
+                     :city "Fribourg"}
+                 :3 {:firstname "Victor"
+                     :age 26
+                     :city "Neuchâtel"}})
 
 ;; text with ring :
 (defn handler [request]
@@ -17,9 +28,9 @@
 
 (jetty/run-jetty handler {:port 12000 :join? false})
 
-;; json with cheshire :
+;; json with aleph :
 (defn handler [request]
-  {:body (json/generate-string people {:pretty true})
+  {:body (json/generate-string people-map {:pretty true})
    :status 200
    :headers {"Content-Type" "application/json"}})
 
