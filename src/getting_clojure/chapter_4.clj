868a092efe12555f713d171c00e32c131134e430
Romain Jysch <rjh-network@pm.me>
2024-02-06 11:08:56 +0100

    feat(clj-training): Rereading chapter 4

--- src/getting_clojure/chapter_4.clj
+++ src/getting_clojure/chapter_4.clj
@@ -2,6 +2,27 @@
 
 ;; Chapter 4 : Logic
 
+(defn print-greeting [preferred-customer]
+  (if preferred-customer
+    (println "Hello my fav!")
+    (println "Hello!")))
+(print-greeting true)
+(print-greeting false)
+
+(defn single-line-print-greeting [preferred-customer]
+  (if preferred-customer "Hello my fav"))
+(single-line-print-greeting true)
+(single-line-print-greeting false) ; return nil
+
+(= 2 2 2 2 2)
+(= 2 2 2 3 2) ; = is identical to the Java equals method
+
+(defn x-bigger-than-y [x y]
+  (if (> x y) "x is bigger than y"))
+(x-bigger-than-y 5 3)
+(x-bigger-than-y 3 3)
+
+
 (def var 1)
 (if (= var 1)
   (println "Good")
@@ -22,6 +43,12 @@
 (vector? 1984)
 (vector? [1984])
 
+(defn shipping-surcharge? [preferred-customer express oversized]
+  (and (not preferred-customer) (or express oversized)))
+(shipping-surcharge? true true true)
+(shipping-surcharge? false true false)
+(shipping-surcharge? false false false)
+
 (if 1
   "I like sf"
   "I like mysteries")
@@ -30,18 +57,38 @@
   "I like sf"
   "I like mysteries")
 
-(if false "I like sf" "I like mysteries")
-(if nil "I like sf" "I like mysteries")
+(if false "I like sf" "I like mysteries") ; only false and nil aren't true
+(if nil "I like sf" "I like mysteries") ; same
+
+;; two falsy things : false and nil ; and infinite number of truthy things
 
 (if 1
   (do
     (println "Bonjour")
     (+ 2 2)))
 
+(defn shipping-fees [prefered-customer order-amount]
+  (if prefered-customer
+    (do
+      (println "Free for the best!")
+      0.00)
+    (do
+      (println "You are not the best, so...")
+      (* 0.1 order-amount)))) ; 10% fees
+(shipping-fees true 100)
+(shipping-fees false 100)
+
 (if 1
   (when
     "Monsieur"
     "Bonjour"))
+
+(defn another-greeting [preferred-customer]
+  (when preferred-customer ; supports multiple statements without needing do
+    (println "Hello returning customer!")
+    (println "Welcome back to Blotts Books!")))
+(another-greeting true)
+(another-greeting false) ; return nil
 
 (defn shipping-charge [preferred-customer? order-amount]
   (if preferred-customer?
@@ -56,12 +103,12 @@
 (shipping-charge false 5)
 (shipping-charge true 1000)
 
-(defn shipping-charge-v2 [preferred-customer? order-amount]
+(defn shipping-charge-v2 [preferred-customer order-amount]
   (cond
-    preferred-customer? 0.0
+    preferred-customer 0.0
     (< order-amount 50) 5.0
     (< order-amount 100) 10.0
-    :else (* 0.1 order-amount)))
+    :else (* 0.1 order-amount))) ; :else is always truthy
 (shipping-charge-v2 false 500)
 (shipping-charge-v2 false 75)
 (shipping-charge-v2 false 5)
@@ -80,7 +127,7 @@
   (case status
     :gold "Welcome gold"
     :silver "Welcome silver"
-    "Welcome"))
+    "Welcome")) ; constant, so not evalued
 (customer-greetings :gold)
 (customer-greetings :silver)
 (customer-greetings :other)
@@ -92,11 +139,15 @@
 (def task)
 (type (def task))
 (defn ensure-task-is-a-vector [task]
-  (if (vector? task) task [task]))
+  (if (vector? task)
+    task [task]))
 (type (ensure-task-is-a-vector task))
 
 (== 5.0 5)
 (= 5.0 5)
+
+(and 1984 "Romain" 1291)
+(and 1984 nil 1291)
 
 (defn equal-or-bigger-than-5 [x]
   (cond
