f35209aff0a8c66364e58eb6aebd9f9bfcc7cc81
Romain Jysch <rjh-network@pm.me>
2024-02-18 20:02:59 +0100

    feat(clj-training): Rereading chapter 8

--- src/getting_clojure/chapter_8.clj
+++ src/getting_clojure/chapter_8.clj
@@ -2,6 +2,9 @@
 
 ;; Chapter 8 : Def, Symbols and Vars
 
+;; def function is perfect for constants
+(def PI 3.14)
+
 (def dune {:title "Dune" :author "Herbert" :isbn "9780441172719"})
 
 (defn book-description [book]
@@ -20,29 +23,53 @@
 (valid-isbn? (dune :isbn))
 (valid-isbn? "978-0441172719")
 
+'author
+'title
+(str 'author) ; turn a symbol in a string
+
 (str 'ISBN-LENGTH)
 'ISBN-LENGTH
 (= 'ISBN-LENGTH 'ISBN-LENGTH)
 (= 'ISBN-LENGTH 'OLD-ISBN-LENGTH)
 
-(def ^:dynamic *debug-enabled* false)
+;; var has two slots : one for the symbol and the other for the value
+(def title "Dune")
+
+(defn compute-pi [diameter]
+  (* PI diameter))
+(compute-pi 2) ; return 6.28
+(def PI 3.14159)
+(compute-pi 2) ; now return 6.28318
+
+;; dynamic var
+(def ^:dynamic *debug-enabled* false) ; * * is convention for dynamic var
 (println *debug-enabled*)
 
 (defn debug [msg]
   (if (= *debug-enabled* true)
-    (println msg)
-    (println "debug disabled")))
+    msg
+    "debug disabled"))
 
 (debug "Hello")
 
+;; binding expression looks a lot like let
 (binding [*debug-enabled* true]
   (debug "hello"))
 
 (def numbers [1 2 3 4 5 6 7 8 9 10])
+(first (next numbers)) ; is like the second function
+(next numbers)
+
 (set! *print-length* 3)
 (println numbers)
+
+(set! *print-length* nil)
+(println numbers)
+
+(binding [*print-length* 5]
+  (println numbers))
 
 (+ 1 1)
 *1
 *2
-*e
+*e ; last exception
