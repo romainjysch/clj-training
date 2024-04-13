e1ae3be6e29188f0194b4019a6df82b7a537d615
Romain Jysch <rjh-network@pm.me>
2024-04-13 09:17:27 +0200

    feat(clj-training): Rereading chapter 10

--- src/getting_clojure/chapter_10.clj
+++ src/getting_clojure/chapter_10.clj
@@ -2,6 +2,9 @@
 
 ;; Chapter 10 : Sequences
 
+(count [1 2 "trois"])
+(count {:un 1 :deux 2 :trois 3})
+
 (def vec-title ["Dune" "Star Wars" "Mission Impossible"])
 (println vec-title)
 (def seq-vec-title (seq vec-title))
@@ -15,13 +18,13 @@
 (def seq-map-title (seq map-title))
 (println seq-map-title)
 
-(seq [])
+(seq []) ; nil
 (seq '())
 (seq {})
 
 (first seq-list-title)
 (first seq-vec-title)
-(rest seq-vec-title)
+(rest seq-vec-title) ; empty sequence if empty
 (next seq-vec-title) ; nil if empty
 (cons "Mario" seq-vec-title)
 
@@ -50,3 +53,96 @@
 (type (rest {:fname "Romain" :lname "Z"}))
 (type (rest {:fname "Romain" :lname "Z"}))
 (type (cons 0 [1 2 3]))
+
+(rest [1 2 3]) ; returns a seq
+(next [1 2 3]) ; returns a seq
+
+(sort vec-title)
+(reverse vec-title)
+(reverse (sort vec-title))
+(partition 1 vec-title)
+
+(def titles ["Dune" "Mission Impossible"])
+(def authors '("Herbert" "Clarke"))
+(interleave titles authors)
+(interpose "and" (interleave titles authors))
+
+(filter neg? [-1 2 -55 -3453 45 3])
+
+(def books
+  [{:title "Deep Six" :price 13.99 :genre :sci-fi :rating 6}
+   {:title "Dracula" :price 1.99 :genre :horror :rating 7}
+   {:title "Emma" :price 7.99 :genre :comedy :rating 9}
+   {:title "2001" :price 10.50 :genre :sci-fi :rating 5}])
+
+(defn cheap?
+  [book]
+  (when (< 10 (:price book))
+    book))
+
+(filter cheap? books) ; filter returns possibliy an empty sequence
+(some cheap? books) ; some returns the first true element or nil
+
+(def some-numbers [1, 53, 811])
+
+(defn doubled
+  [numbers]
+  (map #(* 2 %) numbers))
+(doubled some-numbers)
+
+
+(map (fn [book] (:title book)) books)
+(map :title books)
+(map (fn [book] (count (:title book))) books)
+(map (comp count :title) books)
+(for [b books]
+  (count (:title b)))
+
+(def numbers [10 20 30 40 50])
+(reduce + numbers)
+(reduce + 100 numbers)
+
+(defn higher-price [hi book]
+  (let [price (:price book)]
+    (if (> price hi)
+      price
+      hi)))
+(reduce higher-price 0 books)
+
+(defn format-top-titles
+  [books]
+  (apply
+    str
+    (interpose " // " (map :title (take 3 (reverse (sort-by :rating books)))))))
+(format-top-titles books)
+
+(apply str (interpose " // " (map :title (take 3 (reverse (sort-by :rating books))))))
+
+(defn format-top-titles-v2
+  [books]
+  (->>
+    books
+    (sort-by :rating)
+    reverse
+    (take 3)
+    (map :title)
+    (interpose " // ")
+    (apply str)))
+(format-top-titles-v2 books)
+
+(zipmap [:un :deux :trois] [1 2 3])
+
+(butlast (zipmap [:un :deux :trois] [1 2 3]))
+
+;; A *vector* ending with "Jaws".
+(conj ["Emma" "1984" "The Maze Runner"] "Jaws")
+
+;; A *list* starting with "Jaws".
+(conj '("Emma" "1984" "The Maze Runner") "Jaws")
+
+;; A *seq* starting with "Jaws".
+(cons "Jaws" ["Emma" "1984" "The Maze Runner"])
+
+;; A *seq* starting with "Jaws".
+(cons "Jaws" '("Emma" "1984" "The Maze Runner"))
+
