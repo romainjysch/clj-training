f397240b1b3cdb3d8e72eb86145773c3befccd8b
Romain Jysch <rjh-network@pm.me>
2024-05-04 14:57:16 +0200

    feat(clj-training): Add experiment with partial and complement

--- src/misc.clj
+++ src/misc.clj
@@ -1,5 +1,7 @@
 (ns misc)
 
+;; Anonymous functions :
+
 (def multiply-by-3 #(* 3 %))
 (multiply-by-3 3)
 (multiply-by-3 3)
@@ -9,3 +11,33 @@
 
 (def my-double (fn [x] (* 2 x)))
 (my-double 2)
+
+;; Partial :
+
+(def add-100
+  "100 is the first arg to the + function."
+  (partial + 100))
+(add-100 4)
+(add-100 4 4)
+
+(def multiply-by-100
+  "100 is the first arg to the * function."
+  (partial * 100))
+(multiply-by-100 2)
+(multiply-by-100 2 2)
+
+(def times (partial *))
+(times 1 2 3)
+
+;; Complement :
+
+(map (complement even?) [1 2 3 4])
+(map odd? [1 2 3 4])
+
+(defn fizz?
+  [x]
+  (zero? (rem 5 x)))
+(fizz? 10)
+
+(def reverse-fizz? (complement fizz?))
+(reverse-fizz? 5)
