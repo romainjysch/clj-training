b174de4a3c71d77e9e4f020153ea0a8727453df6
romainjysch <romain.jysch@exoscale.ch>
2025-01-20 08:49:10 +0100

    feat: add Interest is Interesting

--- src/exercism/interest_is_interesting.clj
+++ src/exercism/interest_is_interesting.clj
@@ -12,40 +12,19 @@
     (and (>= balance 1000) (< balance 5000)) 1.621
     :else 2.475))
 
-(interest-rate 200.75M)
-(interest-rate 5000.75M)
-
 ;;
-
-(defn interest-rate-divide-by-100 [balance]
-  (let [interest-rate (interest-rate balance)]
-    (/ interest-rate 100)))
-
-(interest-rate-divide-by-100 200.75M)
-
-(defn annual-interest [balance]
-  (let [multiplier (interest-rate-divide-by-100 balance)]
-    (bigdec (* balance multiplier))))
-
-(annual-interest 200.75M)
 
 (defn annual-balance-update
   "Returns the annual balance update, taking into account the interest rate."
   [balance]
-  (let [annual-interest (annual-interest balance)]
-    (bigdec (+ balance annual-interest))))
-
-(annual-balance-update 200.75M)
-(annual-balance-update 1000.0M)
-(annual-balance-update 898124017.826243404425M)
-(annual-balance-update -0.123M)
-(annual-balance-update -152964.231M)
+  (let [balance (bigdec balance)
+        rate (-> balance interest-rate (/ 100) bigdec)
+        interest (* balance rate)]
+    (if (neg? rate)
+      (- balance interest)
+      (+ balance interest))))
 
 ;;
-
-(def balance 550.5M)
-
-(annual-balance-update balance)
 
 (defn amount-to-donate
   "Returns how much money to donate based on the balance and the tax-free percentage."
@@ -54,4 +33,23 @@
     (if (>= balance 0)
       (int (* balance tax-free-percentage-divide-by-100 2)) 0)))
 
-(amount-to-donate 550.5M 2.5)
+(comment
+  (type 2.45M)
+  (type 2.45)
+  (type (bigdec 2.45))
+  (= 2.45M (bigdec 2.45))
+
+  (interest-rate 200.75)
+  (interest-rate 200.75M)
+  (interest-rate 5000M)
+  (interest-rate 5000.75M)
+  (interest-rate -0.123M)
+
+  (annual-balance-update 200.75M)
+  (annual-balance-update 1000.0M)
+  (annual-balance-update 898124017.826243404425M)
+  (annual-balance-update -0.123M)
+  (annual-balance-update -152964.231M)
+
+  (amount-to-donate 550.5M 2.5)
+  :end)
